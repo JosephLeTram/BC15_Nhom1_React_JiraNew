f0ea3c7f980be9315c93dee555399b0a7b6195f7
JosephLeTram <hungletram@gmail.com>
2022-01-10 20:56:59 +0700

    installed libraries react-router ant-design formik yup

---
+++ src/templates/HomeTemplate/HomeTemplate.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const HomeTemplate = (props) => {
+  const { Component, ...restParam } = props;
+  return <Route {...restParam} render ={(propsRoute)} => {
+      return 
+  }
+};
---
+++ src/templates/HomeTemplate/UserLoginTemplate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+export default function UserLoginTemplate() {
+  return (
+    <div>
+      <hr />
+      <h3>This is User Login Template</h3>
+      <Button type="primary">testing button ant</Button>
+    </div>
+  );
+}
