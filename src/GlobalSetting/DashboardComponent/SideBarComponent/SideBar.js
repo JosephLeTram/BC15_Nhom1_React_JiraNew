7711fd2299bbdf6eb91d66d70897768704d92c80
JosephLeTram <hungletram@gmail.com>
2022-02-14 06:27:37 +0700

    fixed Sidebar using AntDesign

--- src/GlobalSetting/DashboardComponent/SideBarComponent/SideBar.js
+++ src/GlobalSetting/DashboardComponent/SideBarComponent/SideBar.js
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+
+import {
+  UserOutlined,
+  BarsOutlined,
+  PlusOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+
+const { Header, Sider, Content } = Layout;
 
 export default function SideBar() {
+  const [state, setState] = useState({
+    collapsed: false,
+  });
+
+  const toggle = () => {
+    setState({
+      collapsed: !state.collapsed,
+    });
+  };
   return (
-    <div className="sideBar">
-      <div className="sideBar-top">
-        <div className="sideBar-icon">
-          <i className="fab fa-jira" />
+    <div>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={state.collapsed}
+        style={{ height: "100%" }}
+      >
+        <div className="text-right pr-2">
+          <BarsOutlined
+            onClick={toggle}
+            style={{ cursor: "pointer", color: "white", fontSize: "20px" }}
+          />
         </div>
-        <div
-          className="sideBar-icon"
-          data-toggle="modal"
-          data-target="#searchModal"
-          style={{ cursor: "pointer" }}
-        >
-          <i className="fa fa-search" />
-          <span className="title">SEARCH ISSUES</span>
-        </div>
-        <div className="sideBar-icon">
-          <i className="fa fa-plus" />
-          <span className="title">CREATE ISSUES</span>
-        </div>
-      </div>
-      <div className="sideBar-bottom">
-        <div className="sideBar-icon">
-          <i className="fa fa-question-circle" />
-          <span className="title">ABOUT</span>
-        </div>
-      </div>
+
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="3" icon={<UserOutlined />}>
+            Username
+          </Menu.Item>
+          <Menu.Item key="1" icon={<SearchOutlined />}>
+            Search Issues
+          </Menu.Item>
+          <Menu.Item key="2" icon={<PlusOutlined />}>
+            Create Issue
+          </Menu.Item>
+        </Menu>
+      </Sider>
     </div>
   );
 }
